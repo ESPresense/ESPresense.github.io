25d661adc92255bc5357c5bae483d2dc318a86b3
DTTerastar <DT@Terastar.biz>
2021-09-07 08:12:02 -0400

    Add other static parts

---
+++ static/update.sh
@@ -0,0 +1,3 @@
+cp ~/.platformio/packages/tool-esptoolpy/test/images/bootloader_esp32.bin .
+cp ~/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app0.bin .
+cp ../../ESPresense/.pio/build/esp32/partitions.bin .
